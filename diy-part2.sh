a69da2b4483847c22c557e86b63b4c9bc1be50be
jif739519 <65745378+jif739519@users.noreply.github.com>
2020-06-23 10:54:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,25 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 获取luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/diy-packages/luci-app-adguardhome
+# 获取hello world和依赖
+git clone https://github.com/jerrykuku/lua-maxminddb package/diy-packages/helloworld/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/diy-packages/helloworld/luci-app-vssr
+
+# 获取luci-app-openclash
+git clone -b master https://github.com/vernesong/OpenClash package/diy-packages/openclash
+
+#=================================================
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#=================================================
+# 清除旧版argon主题并拉取最新版
+#cd package/lean
+#rm -rf luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+#=================================================
+#=================================================
+# 修改BaiduPCS-web来源
+#rm -rf baidupcs-web
+#git clone https://github.com/liuran001/baidupcs-web-lede baidupcs-web
